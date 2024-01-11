85798c1e59847314ca04d8d47e2a88a4f3ac3a45
mahes <mahes@Mahesh_Seela>
2024-01-11 13:15:13 +0530

    Code format is updated

--- SeleniumGit/src/test/java/com/qa/selenium/practice/GitLoginTest.java
+++ SeleniumGit/src/test/java/com/qa/selenium/practice/GitLoginTest.java
@@ -15,8 +15,8 @@ public class GitLoginTest {
 		driver.manage().window().maximize();
 		driver.get("https://github.com/login");
 		WebElement user = driver.findElement(By.xpath("//input[contains(@name, 'login')]"));
-		if(user.isDisplayed()) {
-			if(user.isEnabled()) {
+		if (user.isDisplayed()) {
+			if (user.isEnabled()) {
 				user.sendKeys("Mahesh");
 				String usertxt = user.getAttribute("value");
 				System.out.println(usertxt);
@@ -25,39 +25,36 @@ public class GitLoginTest {
 				user.sendKeys("Seela");
 				Thread.sleep(1500);
 				user.sendKeys("Mahesh");
-			}
-			else {
+			} else {
 				System.out.println("username field is not enabled");
 			}
-		}
-		else {
+		} else {
 			System.out.println("username field is not displayed");
 		}
-		
+
 		WebElement password = driver.findElement(By.xpath("//input[contains(@id, 'password')]"));
-		if(password.isDisplayed()) {
-			if(password.isDisplayed()) {
+		if (password.isDisplayed()) {
+			if (password.isDisplayed()) {
 				password.sendKeys("Password1!");
 				Thread.sleep(1000);
 			}
 
-				else {
-					System.out.println("Password field is not enabled");
-				}
-			}
 			else {
-				System.out.println("Password field is not displayed");
+				System.out.println("Password field is not enabled");
+			}
+		} else {
+			System.out.println("Password field is not displayed");
 
-	}
+		}
 		WebElement signin = driver.findElement(By.xpath("//input[contains(@type, 'submit')]"));
-		if(signin.isEnabled()) {
+		if (signin.isEnabled()) {
 			signin.click();
 		}
-		String out = driver.findElement(By.xpath("//div[contains(text(), 'Incorrect username or password.')]")).getText();
+		String out = driver.findElement(By.xpath("//div[contains(text(), 'Incorrect username or password.')]"))
+				.getText();
 		Assert.assertEquals(out, "Incorrect username or password.");
 		driver.quit();
 		System.out.println("----- Test Passed ------");
 	}
-	
 
 }
